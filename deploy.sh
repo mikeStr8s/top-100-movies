8cc9001e4e50e3caf0131abf51206879ca49baea
Michael Strating <dev@Mike-intosh.local>
2021-09-17 10:42:41 -0400

    removed dist

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mikeStr8s/top-100-movies.git master:gh-pages
+git push -f https://github.com/mikeStr8s/top-100-movies.git master:gh-pages
 
 cd -
